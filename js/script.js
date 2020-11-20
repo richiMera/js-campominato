6a73dfdd5265d575eaa4520999c885a4229d8ac1
Riccardo Ferrari <richi.f93@gmail.com>
2020-11-20 15:15:45 +0100

    fine js + bonus

--- js/script.js
+++ js/script.js
@@ -16,18 +16,30 @@ function sameNumberCheck(numero, array) {
   return risultato;
 };
 
-function pointCalcolator(array) {
-  for (var i = 0; i < array.length; i++) {
-    alert("hai inserito " + array.length + " numeri su 84");
-    break;
-  }
-};
-
 //-------------------------------------------------------------------------//
+
+var bombs = 16;
+// BONUS
+var level = (prompt("scegli difficoltà: facile, media, difficile"));
+var attempts;
+
+switch (level) {
+  case "difficile":
+   attempts = 50;
+    break;
+  case "media":
+    attempts = 80;
+    break;
+  default:
+    attempts = 100;
+};
+var totalAttempts = attempts - bombs;
+// FINE BONUS
+
 var arrayBomb = [];
 
-while (arrayBomb.length < 16) {
-  var random = generateNumber(1, 100);
+while (arrayBomb.length < bombs) {
+  var random = generateNumber(1, attempts);
   // console.log(random);
 
   var insertNumber = sameNumberCheck(random, arrayBomb);
@@ -38,35 +50,29 @@ while (arrayBomb.length < 16) {
   }
 };
 
-// In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
-// L’utente non può inserire più volte lo stesso numero.
 console.log(arrayBomb);
 
 var userArray = [];
-while (userArray.length < 17) {
-  var userNumbers = parseInt(prompt("inserisci un numero da 1 a 100"));
+var i = false;
+var punteggio = 0;
+while (userArray.length < totalAttempts && i == false) {
+  var userNumbers = parseInt(prompt("inserisci un numero da 1 a " + attempts));
   var numberAlreadyUsed = sameNumberCheck(userNumbers, userArray);
   var checkGameOver = sameNumberCheck(userNumbers, arrayBomb);
   if (checkGameOver == true) {
     alert("Game Over");
-    break;
+    i = true;
   } else if (numberAlreadyUsed == false) {
     userArray.push(userNumbers);
     console.log(userArray);
+    punteggio++;
+    console.log("il punteggio è " +  punteggio);
   };
 };
 
-console.log(userArray);
 
-var finalPointer = pointCalcolator(userArray)
-console.log(userArray);
-// Se il numero è presente nella in arreyBomb, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
-// La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
-// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito
-// var defeatNumber = sameNumberCheck(userNumbers, arrayBomb)
-// if (defeatNumber == true) {
-//   alert("Game Over")
-// } else {
-//
-// }
-// console.log(defeatNumber);
+if (punteggio == 5) {
+  alert("hai vinto");
+} else {
+  alert("il tuo punteggio è " + punteggio);
+};
